75e6828d3692fe40f14f2122acef2a8a02a8d80c
Tombstone2 <hroofid@gmail.com>
2024-03-11 11:40:06 +0700

    feat: add user interface

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import os
+import streamlit as st
 from dotenv import load_dotenv
+import os
+
+def init():
+  load_dotenv()
+
+  # Page Title
+  st.set_page_config(page_title="K.I.M.I Chatbot with Document", page_icon="🤖")
 
 def main():
-  load_dotenv()
-  print(os.getenv('OPENAI_API_KEY'))
+  init()
+
+  # Header
+  st.markdown('<p style="text-align: center; font-size: 40px; font-weight: 600; margin-bottom: 10px;">K.I.M.I</p>', unsafe_allow_html=True)
+  st.markdown('<p style="text-align: center; font-size: 30px; font-weight: 500; margin-bottom: 50px;">Ask me anything with your document</p>', unsafe_allow_html=True)
+
+  with st.sidebar:
+    file_upload = st.file_uploader('Upload your document', type=['pdf', 'csv'])
+    if file_upload is not None:
+      st.write(file_upload.type == 'application/pdf')
+
 
 
 if __name__ == '__main__':
